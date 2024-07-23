338b524fa44b67c9c388de051329d03fd1ebe9eb
pnwheeler <pnwheeler96@gmail.com>
2024-07-22 23:33:27 -0400

    updated code styles, text

---
+++ _app/immutable/chunks/stores.e4754bf1.js
@@ -0,0 +1,1 @@
+import{w as i}from"./index.dd68086b.js";function c(){const{subscribe:t,update:e}=i("center");return{subscribe:t,setDirection:r=>e(n=>r)}}const s=c();export{s as d};
---
+++ _app/immutable/nodes/6.cf08fbe8.js
@@ -0,0 +1,1 @@
+import{P as e,_ as n}from"../chunks/6.de55fffb.js";export{e as component,n as universal};
